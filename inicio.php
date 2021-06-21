6ec84ed309d95871583c63a54499c6c0dfcb0fb4
perezj861 <perezj861@gmail.com>
2021-06-21 11:30:01 -0300

    tratatando de hacer andar el boton captcha en inicio.php, sin aun haber pasado lo de fran desde principal.php

--- inicio.php
+++ inicio.php
@@ -26,8 +26,59 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+ <!-- parte 1 del tuto para el captcha -->
+ 
+ <script
+	  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+	  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+	  crossorigin="anonymous"></script>
+ 
+	<!-- Cambia 6LcZu9QUAAAAACaj-WBiVIQUlr94vfCC8DUpIanS por tu clave de sitio web -->
+    <script src="https://www.google.com/recaptcha/api.js?render=6Lc6EEgbAAAAAGtycCRxOfcqB3Su3bQI8LTbn7ul"></script>
 
 
+	<script>
+    $('#form').submit(function(event) {
+        event.preventDefault();
+        /*Cambia 6LcZu9QUAAAAACaj-WBiVIQUlr94vfCC8DUpIanS por tu clave de sitio web*/
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LcZu9QUAAAAACaj-WBiVIQUlr94vfCC8DUpIanS', {action: 'registro'}).then(function(token) {
+                $('#form').prepend('<input type="hidden" name="token" value="' + token + '">');
+                $('#form').prepend('<input type="hidden" name="action" value="registro">');
+                $('#form').unbind('submit').submit();
+            });;
+        });
+  });
+  </script>
+ <!-- parte 2 del tuto para el captcha -->
+ 
+	<?php
+		
+		define("RECAPTCHA_V3_SECRET_KEY", '6Lc6EEgbAAAAAGtycCRxOfcqB3Su3bQI8LTbn7ul');
+
+		$token = $_POST['token'];
+		$action = $_POST['action'];
+		
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		$arrResponse = json_decode($response, true);
+		
+		
+		if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+			
+			echo "ok!, eres un humano";
+		} else {
+			
+			echo "Lo siento, parece que eres un Robot";
+		}
+
+?>
 	<script type="text/javascript">
 		$(function () {
 			$('[data-toggle="tooltip"]').tooltip()
